cd55f8634a61ff162da48718b7c74563a3b750d5
Hamza Rizvi <m.hamza.asad.22@gmail.com>
2024-12-14 03:48:39 +0500

    feat: font update + markdown support

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -2,6 +2,8 @@ import { useLocation, useParams } from "wouter";
 import Calendar, { monthNames } from "../components/calendar";
 import HomeLayout from "../layouts/home";
 import { useEvents } from "../components/contexts/events";
+import { useRemark } from "react-remark";
+import { useEffect } from "react";
 
 // courses and workshops
 function Events() {
@@ -23,17 +25,52 @@ export function EventPage() {
   const params = useParams();
   const { getEventById } = useEvents();
 
+  const [eventText, setEventText] = useRemark();
+
   const [_, navigate] = useLocation();
 
   const event = getEventById!(params.id || "");
 
+  //possible bug, using images crashes the page.
+
+  useEffect(() => {
+    setEventText(`### Welcome to Kitab Ghar`);
+  });
+
+  //check the event Payment Options against the data
+
   return (
     <HomeLayout>
-      <div>
+      <div className="flex flex-col items-center align-center justify-between mx-10 md:mx-64 my-5">
         <h1>{event?.name}</h1>
-        <p>{event?.description || "No description provided"}</p>
+        <div className="markdown w-full">
+          {
+            /*TODO : We can get the list of all images and display them together perhaps ? */
+            eventText
+          }
+        </div>
+
+        <div className="m-4">
+          <h4 className="font-bold text-lg">Payment Options</h4>
+          <ul className="list-decimal">
+            <li className="my-2">
+              <h5 className="font-bold">Bank Transfer</h5>
+              <p>
+                Transfer payment to the below bank account and we'll email you
+                the tickets shortly
+              </p>
+            </li>
+            <li className="my-2">
+              <h5>{"EasyPaisa - (WIP)"}</h5>
+              <button className="disabled p-2 bg-gray-400 text-white rounded my-1">
+                Pay Now
+              </button>
+            </li>
+          </ul>
+        </div>
+
         <button
-          className="bg-brand-pink p-2 rounded border-2 border-brand-black self-start"
+          className="self-end bg-brand-pink p-4 text-xl rounded border-2 border-brand-black self-start shadow-[2px_2px_0px_rgba(0,0,0,0.5)] transition-shadow duration-300 hover:shadow-[5px_5px_0px_rgba(0,0,0,0.5)]"
           onClick={() => navigate(`/event/${params.id}/register`)}
         >
           Get Tickets
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ['"Baloo 2"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       colors: {
         sidebar: {
